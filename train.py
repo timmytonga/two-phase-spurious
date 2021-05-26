a26fe1934b43c7200e9ed469e2d33afe1ed6e6ec
Thien <nguyen.thien@northeastern.edu>
2021-05-26 04:35:36 -0400

    added second phase, resume, logging, and stuff

---
+++ train.py
@@ -0,0 +1,122 @@
+import torch
+from tqdm.auto import tqdm
+import os
+import csv
+from pprint import pprint
+import numpy as np
+
+
+def write_to_writer(writer, content):
+    writer.writerow(content)
+    pprint(content)
+
+
+# refactor by making only 1 writer
+def run_epoch(epoch, model, device, optimizer, loader, loss_computer, writer, logger, is_training):
+    if is_training:
+        model.train()
+    else:
+        model.eval()
+
+    running_loss, total_margin = 0, 0  # keep track of avg loss, margin in train
+    l_correct, g_correct, total = 0, 0, 0  # for validation
+    # g = group and l = label
+    group_track = {f'g{i}': {'correct_g': 0, 'total': 0, 'margin': 0, 'correct_l': 0} for i in
+                   range(4)}  # keeps track of counts of #correct, #total for each group g0-g4
+    log_train_every = 200
+    with torch.set_grad_enabled(is_training):
+        for batch_idx, batch in enumerate(tqdm(loader)):
+            batch = tuple(t.to(device) for t in batch)
+            x, y, g = batch
+            outputs = model(x)
+
+            if is_training:
+                optimizer.zero_grad()
+                loss = loss_computer(outputs, g)  # we are classifying groups
+                loss.backward()
+                optimizer.step()
+                # print statistics
+                running_loss += loss.item()
+                if (batch_idx % log_train_every) == log_train_every - 1:  # print every 200 mini-batches
+                    logger.write('[%d, %5d] loss: %.3f, avg_margin: %.3f' %
+                                 (epoch, batch_idx + 1, running_loss / log_train_every, total_margin / total))
+                    running_loss = 0.0
+
+            # extra validation
+            with torch.no_grad():
+                # accuracies and margins
+                total += g.size(0)  # total
+                _, predicted = torch.max(outputs.data, 1)  # get predictions
+                maskg = torch.zeros((len(g), 4), dtype=torch.bool, device=device)
+                maskg[np.arange(len(g)), g] = 1
+                margins = outputs[maskg] - torch.max(outputs * (~maskg), dim=1)[0]
+                total_margin += margins.sum().item()
+                label_pred = (predicted // 2 == y)
+                for g_idx in range(4):
+                    group_track[f'g{g_idx}']['correct_g'] += (
+                        (predicted == g)[g == g_idx]).sum().item()  # correctly predict
+                    group_track[f'g{g_idx}']['correct_l'] += label_pred[g == g_idx].sum().item()
+                    group_track[f'g{g_idx}']['total'] += (
+                            g == g_idx).sum().item()  # total number of group's instance encountered
+                    group_track[f'g{g_idx}']['margin'] += margins[g == g_idx].sum().item()
+                g_correct += (predicted == g).sum().item()
+                l_correct += label_pred.sum().item()
+        # write stats in dict and csv
+        stats_dict = {'epoch': epoch, 'total_acc': f"{l_correct / total:.4f}", 'split_acc': f'{g_correct / total:.4f}',
+                      'avg_margin': f"{total_margin / total:.4f}"}
+        for g in range(4):
+            stats_dict[f'group{g}_acc'] = f"{group_track[f'g{g}']['correct_g'] / group_track[f'g{g}']['total']:.4f}"
+            stats_dict[f'total_acc:g{g}'] = f"{group_track[f'g{g}']['correct_l'] / group_track[f'g{g}']['total']:.4f}"
+            stats_dict[f'group{g}_margin'] = f"{group_track[f'g{g}']['margin'] / group_track[f'g{g}']['total']:.4f}"
+        if is_training:
+            stats_dict['loss'] = f'{running_loss / log_train_every:.4f}'
+
+        write_to_writer(writer, stats_dict)
+
+
+def train(args, model, device, mode, data, logger):
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.SGD(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=args.lr,
+        momentum=0.9,
+        weight_decay=args.weight_decay)
+
+    train_path = open(os.path.join(args.log_dir, 'train.csv'), mode)
+    val_path = open(os.path.join(args.log_dir, 'val.csv'), mode)
+    test_path = open(os.path.join(args.log_dir, 'test.csv'), mode)
+
+    total_acc_per_group = [f'total_acc:g{i}' for i in range(4)]
+    train_columns = ['epoch', 'total_acc', 'group0_acc', 'group1_acc', 'group2_acc', 'group3_acc', 'split_acc', 'loss',
+                     'avg_margin', 'group0_margin',
+                     'group1_margin', 'group2_margin', 'group3_margin'] + total_acc_per_group
+
+    valtest_columns = ['epoch', 'total_acc', 'group0_acc', 'group1_acc', 'group2_acc', 'group3_acc', 'split_acc',
+                       'avg_margin', 'group0_margin',
+                       'group1_margin', 'group2_margin', 'group3_margin'] + total_acc_per_group
+
+    train_writer = csv.DictWriter(train_path, fieldnames=train_columns)
+    val_writer = csv.DictWriter(val_path, fieldnames=valtest_columns)
+    test_writer = csv.DictWriter(test_path, fieldnames=valtest_columns)
+    if mode == 'w':
+        val_writer.writeheader()
+        train_writer.writeheader()
+        test_writer.writeheader()
+
+    train_loader, val_loader, test_loader = data['train_loader'], data['val_loader'], data['test_loader']
+    for epoch in range(args.resume_from, args.resume_from + args.n_epochs):
+        # train
+        logger.write(f'Train epoch {epoch}')
+        run_epoch(epoch + 1, model, device, optimizer, train_loader, criterion, train_writer, logger, is_training=True)
+
+        # validate
+        logger.write(f'Validate epoch {epoch}')
+        run_epoch(epoch + 1, model, device, optimizer, val_loader, criterion, val_writer, logger, is_training=False)
+
+    # Save model
+    save_path = os.path.join(args.log_dir, f'model_{args.resume_from + args.n_epochs}.pth')
+    torch.save(model.state_dict(), save_path)
+
+    train_path.close()
+    val_path.close()
+    test_path.close()
--- train_classifier.py
+++ train_classifier.py
@@ -1,96 +1,96 @@
-from data.data import log_data
-from utils import set_seed, Logger, log_args
-import argparse
 import os
+import argparse
+from utils import set_seed, Logger, log_args
 import torch
-from variable_width_resnet import resnet10vw
 from data.celebA_dataset import CelebADataset
 from data.dro_dataset import DRODataset
+from data.data import log_data
+from variable_width_resnet import resnet10vw
 from train import train
 
 
 def main():
+    sampling_methods = ['subsample', 'reweight']
+
     parser = argparse.ArgumentParser()
-    # args: log_dir, seed, lr, batchsize, width, n_epochs, weight_decay
-    parser.add_argument('--log_dir', default='./logs')
-    parser.add_argument('--seed', type=int, default=0)
-    parser.add_argument('--n_epochs', type=int, default=4)
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--weight_decay', type=float, default=0.00001)
     parser.add_argument('--resnet_width', type=int, default=None)
-    parser.add_argument('--reweight_groups', action='store_true', default=False)
-    parser.add_argument('--resume', action='store_true', default=False)
+    parser.add_argument('--model_path', type=str)
+    parser.add_argument('--lr', type=float, default='1e-3')
+    parser.add_argument('--weight_decay', type=float, default='1e-6')
+    parser.add_argument('--sampling_method', choices=sampling_methods, default=None)
+    parser.add_argument('--n_epochs', type=int, default=25)
+    parser.add_argument('--log_dir', type=str, default='./phase2_log')
+    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--resume_from', type=int, default=0)
 
+    # '--dataset', 'CelebA', '-s', 'confounder', '-t', 'Blond_Hair', '-c', 'Male', '--log_dir', 'twophaselog_w32', '--seed', '0',
+    # toparse = ['--resnet_width', '16',
+    #            '--model_path', 'log_w16_seed0/last_model_50.pth',
+    #            '--sampling_method', 'reweight']
     args = parser.parse_args()
+    assert args.resume_from == 0, "HAVE NOT IMPLEMENTED RESUME_FROM != 0...DO NOT SET!"
     # setup logging
-    mode = 'a'
-    if not args.resume:
-        args.resume_from = 0
-        mode = 'w'
+    mode = 'w'
     if not os.path.exists(args.log_dir):
         os.makedirs(args.log_dir)
     logger = Logger(os.path.join(args.log_dir, 'log.txt'), mode)
     # Record args
-    logger.write(f'Train_jointly CelebA')
     log_args(args, logger)
     set_seed(args.seed)
 
-    # loading data
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('Using {} device'.format(device))
 
     root_dir = '/home/thiennguyen/research/datasets/celebA/'  # dir that contains data
     target_name = 'Blond_Hair'  # we are classifying whether the input image is blond or not
     confounder_names = ['Male']  # we aim to avoid learning spurious features... here it's the gender
-    model_type = 'resnet10vw'  # what model we are using to process --> this is to determine the input size to
-    # rescale the image
+    model_type = 'resnet10vw'
     augment_data = False
-
     fraction = 1.0
     splits = ['train', 'val', 'test']
+
     full_dataset = CelebADataset(root_dir=root_dir,
                                  target_name=target_name,
                                  confounder_names=confounder_names,
                                  model_type=model_type,
-                                 # this string is to get the model's input size (for resizing) and input type (image
-                                 # or precomputed)
                                  augment_data=augment_data)  # augment data adds random resized crop and random flip.
 
     subsets = full_dataset.get_splits(
         # basically return the Subsets object with the appropriate indices for train/val/test
         splits,  # also implements subsampling --> just remove random indices of the appropriate groups in train
         train_frac=fraction,  # fraction means how much of the train data to use --> randomly remove if less than 1
-        subsample_to_minority=False)
-    train_data, val_data, test_data = [
+        subsample_to_minority=(args.sampling_method == 'subsample'))
+
+    dro_subsets = [
         DRODataset(
             subsets[split],  # process each subset separately --> applying the transform parameter.
             process_item_fn=None,
             n_groups=full_dataset.n_groups,
             n_classes=full_dataset.n_classes,
-            group_str_fn=full_dataset.group_str) \
-        for split in
-        splits]  # group_str is a function that takes an int representing a group in the valid range and return the
-    # group's name
-    loader_kwargs = {'batch_size': args.batch_size, 'num_workers': 4, 'pin_memory': True}
-    train_loader = train_data.get_loader(train=True, reweight_groups=args.reweight_groups, **loader_kwargs)
-    val_loader = val_data.get_loader(train=False, reweight_groups=None, **loader_kwargs)
-    test_loader = test_data.get_loader(train=False, reweight_groups=None, **loader_kwargs)
+            group_str_fn=full_dataset.group_str)
+        for split in splits]
+
+    train_data, val_data, test_data = dro_subsets
+    train_loader = train_data.get_loader(train=True, reweight_groups=(args.sampling_method == 'reweight'), batch_size=128)
+    val_loader = val_data.get_loader(train=False, reweight_groups=None, batch_size=128)
+    test_loader = test_data.get_loader(train=False, reweight_groups=None, batch_size=128)
     data = {'train_loader': train_loader, 'val_loader': val_loader, 'test_loader': test_loader,
             'train_data': train_data, 'val_data': val_data, 'test_data': test_data}
     n_classes = 4  # since we are classifying the groups here
     log_data(data, logger)
-    
-    logger.flush()
-    # initialize model, loss, and optimizer
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    logger.write('Using {} device'.format(device))
+    # load saved model
+
     model = resnet10vw(args.resnet_width, num_classes=n_classes)
-    if args.resume:
-        load_path = os.path.join(args.log_dir, f'model_{args.resume_from}.pth')
-        assert os.path.exists(load_path), f"Model path {load_path} specified does not exist."
-        model.load_state_dict(torch.load(load_path))
-        logger.write(f"Loaded model from {load_path} successfully! Training...")
+    model.load_state_dict(torch.load(args.model_path))
     model.to(device)
+
+    # freeze everything except the last layer
+    for name, param in model.named_parameters():
+        if name not in ['fc.weight', 'fc.bias']:
+            param.requires_grad = False
+    # make sure freezing really worked
+    parameters = list(filter(lambda p: p.requires_grad, model.parameters()))
+    assert len(parameters) == 2  # fc.weight, fc.bias
 
     train(args, model, device, mode, data, logger)  # train model, save it, and log stats
 
